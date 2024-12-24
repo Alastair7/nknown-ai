6e9b723fba8382727091af070526b6707a4688d7
Alastair7 <alexandru.ban.drmer@gmail.com>
2024-12-24 17:37:54 +0100

    add conversation-like interaction in the terminal

--- terminaitor.py
+++ terminaitor.py
@@ -9,26 +9,42 @@ client = openai.OpenAI(
     base_url="https://glhf.chat/api/openai/v1"
 )
 
+def create_message(role, content):
+    return {"role": role, "content": content}
+
+meta_prompt = """
+WRITE YOUR META-PROMPT HERE. eg: You're a kind assistant.
+"""
 messages = [
-        {"role": "system", "content": "Test"},
-        {"role": "user", "content": "Hey!"}
+        {"role": "system", "content": meta_prompt}
         ]
 
-completion = client.chat.completions.create(
-    model="hf:mistralai/Mistral-7B-Instruct-v0.3",
-    messages=messages
-)
 
-# stop_terminaitor = False
+stop_terminaitor = False
 
-# while stop_terminaitor == False:
-#     prompt = str(input('Prompt >> '))
+while stop_terminaitor == False:
+    prompt = str(input('Prompt >> '))
+    print('\n')
 
-#     if prompt == 'stop':
-#         stop_terminaitor = True
-#     else:
-#         messages.append(prompt)
+    if prompt == 'stop':
+        stop_terminaitor = True
+        break
+    else:
+        message = create_message('user', prompt)
+        messages.append(message)
+    
+    completion = client.chat.completions.create(
+        stream=True,
+        model="hf:mistralai/Mistral-7B-Instruct-v0.3",
+        messages=messages
+    )
+
+    print('ASSISTANT NAME: ')
+    for chunk in completion:
+        if chunk.choices[0].delta.content is not None:
+            print(chunk.choices[0].delta.content, end='', flush=True)
+    
+    print('\n')
 
 
-print(completion.choices[0].message.content)
 
